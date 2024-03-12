06d7eb2518fce1c2e8f6b682a4a179ba02a50031
RobiBlocks <142887221+RobiBlocks@users.noreply.github.com>
2024-03-12 10:28:10 +0100

    Add files via upload

---
+++ LINQ/LINQtoSQL/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Linq;
+
+namespace LINQtoSQL
+{
+    [Table(Name = "iPhones")]
+    public class iPhone
+    {
+        private string _name;
+        [Column(Storage = "_name")]
+        public string name
+        {
+            get { return this._name; }
+            set { this._name = value; }
+        }
+
+        private string _release;
+        [Column(Storage = "_release")]
+        public string release
+        {
+            get { return this._release; }
+            set { this._release = value; }
+        }
+
+        private string _processor;
+        [Column(Storage = "_processor")]
+        public string processor
+        {
+            get { return this._processor; }
+            set { this._processor = value; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataContext db = new DataContext
+                (@"C:\LINQtoSQLdata\Apple.mdf");
+            Table<iPhone> iPhones = db.GetTable<iPhone>();
+
+            db.Log = Console.Out;
+            IQueryable<iPhone> ShowiPhones =
+                from phone in iPhones
+                where phone.processor == "A14 Bionic"
+                select phone;
+
+            foreach (iPhone phone in ShowiPhones)
+            {
+                Console.WriteLine(phone.name, phone.release, phone.processor);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+
